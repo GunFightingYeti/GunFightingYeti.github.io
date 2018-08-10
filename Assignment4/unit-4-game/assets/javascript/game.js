b167c8590a9d7bcddfa5646359d0881e0ef9c208
GunFightingYeti <gunfightingyeti@gmail.com>
2018-08-09 22:53:19 -0600

    Added stats and onclick choice.

--- Assignment4/unit-4-game/assets/javascript/game.js
+++ Assignment4/unit-4-game/assets/javascript/game.js
@@ -1,50 +1,3 @@
-//Character objects
-var luke = {
-    name: "Luke Skywalker",
-    health: 175,
-    attack: 20,
-    power: 6,
-    image: "assets/images/luke.png"
-}
-
-$("lattack").text(luke.attack);
-
-var obiwan = {
-    name: "Obiwan Kenobi",
-    health: 200,
-    attack: 15,
-    power: 7,
-    image: "assets/images/obiwan.jpg"
-}
-var yoda = {
-    name: "Yoda",
-    health: 150,
-    attack: 25,
-    power: 8,
-    image: "assets/images/yoda.jpg"
-}
-var vader = {
-    name: "Darth Vader",
-    health: 175,
-    attack: 20,
-    power: 7,
-    image: "assets/images/vader.jpg"
-}
-var maul = {
-    name: "Darth Maul",
-    health: 200,
-    attack: 15,
-    power: 6,
-    image: "assets/images/maul.png"
-}
-var sidious = {
-    name: "Darth Sidious",
-    health: 125,
-    attack: 25,
-    power: 8,
-    image: "assets/images/sidious.jpg"
-}
-
 //On ready - Open
 $(document).ready(function () {
 
@@ -62,9 +15,76 @@ $(document).ready(function () {
     }
     //Run gamestart program
     gamestart()
-
     //New game - Close
 
+
+    //Character objects - Start
+    //Luke
+    var luke = {
+        name: "Luke Skywalker",
+        health: 175,
+        attack: 20,
+        power: 6,
+        image: "assets/images/luke.png"
+    };
+    $(".lattack").text(luke.attack);
+    $(".lhealth").text(luke.health);
+
+    //Obiwan
+    var obiwan = {
+        name: "Obiwan Kenobi",
+        health: 200,
+        attack: 15,
+        power: 7,
+        image: "assets/images/obiwan.jpg"
+    };
+    $(".oattack").text(obiwan.attack);
+    $(".ohealth").text(obiwan.health);
+
+    //Yoda
+    var yoda = {
+        name: "Yoda",
+        health: 150,
+        attack: 25,
+        power: 8,
+        image: "assets/images/yoda.jpg"
+    };
+    $(".yattack").text(yoda.attack);
+    $(".yhealth").text(yoda.health);
+
+    //Darth Vader
+    var vader = {
+        name: "Darth Vader",
+        health: 175,
+        attack: 20,
+        power: 7,
+        image: "assets/images/vader.jpg"
+    };
+    $(".vattack").text(vader.attack);
+    $(".vhealth").text(vader.health);
+
+    //Darth Maul
+    var maul = {
+        name: "Darth Maul",
+        health: 200,
+        attack: 15,
+        power: 6,
+        image: "assets/images/maul.png"
+    };
+    $(".mattack").text(maul.attack);
+    $(".mhealth").text(maul.health);
+
+    //Darth Sidious
+    var sidious = {
+        name: "Darth Sidious",
+        health: 125,
+        attack: 25,
+        power: 8,
+        image: "assets/images/sidious.jpg"
+    };
+    $(".sattack").text(sidious.attack);
+    $(".shealth").text(sidious.health);
+    //Character objects - Close
 
     //Navigation - Open
     //Choose to Hero
@@ -79,49 +99,121 @@ $(document).ready(function () {
         $("#darkside").show();
     });
 
-    // Hero to Opponent
-    // Show Dark Opponent
-    $(".lighthero").click(function () {
+
+    // Hero to Opponent - Open
+    // Light side choice
+    // Hero = Obiwan
+    $(".heroben").click(function () {
+        $(this).appendTo("#hero");
         $("#lightside").hide();
         $("#darksideOpp").show();
     });
-    // Show Dark Opponent
-    $(".darkhero").click(function () {
+    // Hero = Luke
+    $(".heroluke").click(function () {
+        $(this).appendTo("#hero");
+        $("#lightside").hide();
+        $("#darksideOpp").show();
+    });
+    // Hero = Yoda
+    $(".heroyoda").click(function () {
+        $(this).appendTo("#hero");
+        $("#lightside").hide();
+        $("#darksideOpp").show();
+    });
+
+    // Dark side choice
+    // Hero = Vader
+    $(".herovader").click(function () {
+        $(this).appendTo("#hero");
         $("#darkside").hide();
         $("#lightsideOpp").show();
     });
+    // Hero = Maul
+    $(".heromaul").click(function () {
+        $(this).appendTo("#hero");
+        $("#darkside").hide();
+        $("#lightsideOpp").show();
+    });
+    // Hero = Sidious
+    $(".herosidious").click(function () {
+        $(this).appendTo("#hero");
+        $("#darkside").hide();
+        $("#lightsideOpp").show();
+    });
+    // Hero to Opponent - Close
 
-    //Opponent to fight
-    //Dark oppenent to fight
-    $(".darkOpp").click(function () {
-        $("#darksideOpp").hide();
+
+    //Opponent to fight - Open
+    //Light side opponent
+    //Opponent = Obiwan
+    $(".oppben").click(function () {
+        $(this).appendTo("#opponent");
+        $("#lightsideOpp").hide();
         $("#fight").show();
     });
-    //Light opponent to fight
-    $(".lightOpp").click(function () {
+    //Opponent = Luke
+    $(".oppluke").click(function () {
+        $(this).appendTo("#opponent");
+        $("#lightsideOpp").hide();
+        $("#fight").show();
+    });
+    //Opponent = Yoda
+    $(".oppyoda").click(function () {
+        $(this).appendTo("#opponent");
         $("#lightsideOpp").hide();
         $("#fight").show();
     });
 
-    //Fight to lose
-    //New Opponent
-     $(".newOpp").click(function () {
-         $("#fight").hide();
-         $("#lose").show();
-     });
-
-    //Fight to win
-    //New Opponent
-    $(".newOpp").click(function () {
-        $("#fight").hide();
-        $("#win").show();
+    //Dark side opponent
+    //Opponent = Vader
+    $(".oppvader").click(function () {
+        $(this).appendTo("#opponent");
+        $("#darksideOpp").hide();
+        $("#fight").show();
     });
+    //Opponent = Maul
+    $(".oppmaul").click(function () {
+        $(this).appendTo("#opponent");
+        $("#darksideOpp").hide();
+        $("#fight").show();
+    });
+    //Opponent = Sidious
+    $(".oppsidious").click(function () {
+        $(this).appendTo("#opponent");
+        $("#darksideOpp").hide();
+        $("#fight").show();
+    });
+    //Opponent to fight - Close
+
+
+    //Fight Button
+    var battle =
+        $(".fight").click(function () {
+            $("#info").append(battle)
+        });
+
+    // If dead then you lose, else you are alive and then yay! 
+    if (hero.health <= 0) {
+        //Fight to lose
+        //New Opponent
+        $(".newOpp").click(function () {
+            $("#fight").hide();
+            $("#lose").show();
+        });
+    } else {
+
+        //Fight to win
+        //New Opponent
+        $(".newOpp").click(function () {
+            $("#fight").hide();
+            $("#win").show();
+        });
+    }
 
     //New Game
     //Restart
     $(".newgame").click(function () {
         gamestart();
-
     });
     //Navigation - Close
 
