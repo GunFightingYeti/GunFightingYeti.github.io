0942decaff0e44afbc9a3047c9b5f860c506594c
GunFightingYeti <gunfightingyeti@gmail.com>
2018-08-13 20:36:46 -0600

    Started JS.

---
+++ Assignment5/assets/javascript/java.js
@@ -0,0 +1,44 @@
+var q1 = {
+    question: "What is the name of the artificial intelligence that is breifly housed in a potato?",
+    answer1: "Phillis",
+    answer2: "ArGon",
+    answer3: "GLaDOS",
+    answer4: "Atlas"
+};
+
+var q2 = {
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: ""
+};
+
+var q3 = {
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: ""
+};
+
+var q4 = {
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: ""
+};
+
+var q5 = {
+    question: "",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: ""
+};
+
+var trivia = [q1, q2, q3, q4, q5];
+
+console.log(trivia)
+
