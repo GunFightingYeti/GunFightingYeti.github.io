dcd2b025dcb428e263006cc3cfd1e58e981b5c39
GunFightingYeti <gunfightingyeti@gmail.com>
2018-08-20 00:08:01 -0600

    Created Assignment6 folder and began populating.

---
+++ Assignment6/README.md
@@ -0,0 +1,19 @@
+Giphy a Break
+Assignment 6 of the DU full stack developer bootcamp.  Dynamically creating and modifying HTML with Javascript and Jquery while making AJAX calls to the Giphy API and displaying results.
+
+Getting Started
+Load the index.html file in a web browser.
+
+Prerequisites
+Web browser installed on your machine.
+
+Built With
+Visual Studio Code
+Google Chrome
+Deployed with Github
+
+Authors
+Tim Lewis
+
+Acknowledgments
+DU coding bootcamp for teaching me so much about coding.
---
+++ Assignment6/assets/javascript/java.js
@@ -0,0 +1,57 @@
+
+
+//Animal gif button classwork - open
+$(".button").on("click", function () {
+    var animal = $(this).attr("data-animal");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET",
+        data: {
+                q: animal,
+                tag: "animal",
+                apikey: "dc6zaTOxFJmzC",
+                limit: 10
+            }
+    }).then(function (response) {
+        // Step 1: Run this file, click a button, and see what the response object looks like in the browser's console.
+        // Open up the data key, then open up the 0th, element. Study the keys and how the JSON is structured.
+
+        console.log(response);
+
+        // Step 2: since the image information is inside of the data key,
+        // make a variable named results and set it equal to response.data
+
+        // =============== put step 2 in between these dashes ==================
+        var results = response.data;
+        // ========================
+
+        for (var i = 0; i < results.length; i++) {
+
+            // Step 3: uncomment the for loop above and the closing curly bracket below.
+            // Make a div with jQuery and store it in a variable named animalDiv.
+            var animalDiv = $("<div class='item'>");
+            // Make a paragraph tag with jQuery and store it in a variable named p.
+            var p = $("<p>");
+            // Set the inner text of the paragraph to the rating of the image in results[i].
+            p.text("Rating: " + results[i].rating.toUpperCase());
+            // Make an image tag with jQuery and store it in a variable named animalImage.
+            var animalImage = $("<img>");
+            // Set the image's src to results[i]'s fixed_height.url.
+            animalImage.attr("src", results[i].images.fixed_height.url);
+            // Append the p variable to the animalDiv variable.
+            animalDiv.prepend(p);
+            // Append the animalImage variable to the animalDiv variable.
+            animalDiv.prepend(animalImage);
+            // Prepend the animalDiv variable to the element with an id of gifs-appear-here.
+            $("#gifs-appear-here").prepend(animalDiv);
+            // ============= put step 3 in between these dashes ======================
+
+            // ==================================
+        }
+
+    });
+});
+//Animal gif button classwork - close
+
