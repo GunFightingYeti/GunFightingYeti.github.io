435dd6be9b969283616f55be10e1af57088b9e4d
GunFightingYeti <gunfightingyeti@gmail.com>
2018-08-22 00:35:36 -0600

    Still working on several things.  Code is not really working yet.

--- Assignment6/assets/javascript/java.js
+++ Assignment6/assets/javascript/java.js
@@ -1,12 +1,18 @@
 $(document).ready(function () {
 
     // Initial array of subjects
-    var subjects = ["Portal 2", "Firefly", "Star Wars", "Jackie Chan", "Adventure Time"];
+    var subjects = ["Adventure Time", "Finn the Human", "Jake the Dog", "Princess Bubblegum", "The Ice King"];
+
+    // Function to clear the input box
+    function inputClear() {
+        $("#input").val('');
+    };
 
     // Function for displaying new buttons
     function makeButtons() {
         $("#buttons").empty();
 
+        //Loop through the array and create a button for each item
         for (var i = 0; i < subjects.length; i++) {
             var btnDiv = $("<button>");
             btnDiv.addClass("subject button");
@@ -16,19 +22,16 @@ $(document).ready(function () {
         }
     };
 
-    makeButtons();
-
-    // Function to clear the input box after pressing enter
-    function inputClear() {
-        $("#input").val('');
-    };
-
+    //Submit button function
     $("#submit").on("click", function (event) {
+        event.preventDefault();
 
+        //If input field is empty then alert
         if ($("#input").val() === "") {
             alert("Input field cannot be empty - Please type something in the box");
+
+            //Else, add input text to subjects array
         } else {
-            event.preventDefault();
             var name = $("#input").val().trim();
             subjects.push(name);
             makeButtons();
@@ -36,34 +39,64 @@ $(document).ready(function () {
         };
     });
 
+    //Run the makeButtons function to create the initial buttons 
+    makeButtons();
+
     //Call the gif from pushing a button
     $(".subject").on("click", function () {
-        var queryURL = "https://api.giphy.com/v1/gifs/random?";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?";
         var search = $(this).attr("data-name");
+        $("#gifs").empty();
 
+        //AJAX query
         $.ajax({
             url: queryURL,
             method: "GET",
             data: {
                 q: search,
+                rating: "g",
                 limit: 10,
+                lang: "en",
                 apikey: "TgrLk2W8P5SVKS4dfmvCAwhNGY7Gdbpb"
             }
         }).then(function (response) {
+            console.log(response.data);
+            //Make the return into a variable
             var results = response.data;
 
+            //Display the return gif on the page
             for (var i = 0; i < results.length; i++) {
+
                 var inputDiv = $("<div class='item'>");
                 var p = $("<p>");
                 p.text("Rating: " + results[i].rating.toUpperCase());
                 var inputImage = $("<img>");
                 inputImage.attr("src", results[i].images.fixed_height.url);
+                inputImage.attr("data-still", results[i].images.fixed_height_still.url);
+                inputImage.attr("data-animate", results[i].images.fixed_height.url);
+                inputImage.attr("data-state", "still");
+                inputImage.attr("class", "gif");
                 inputDiv.prepend(p);
                 inputDiv.prepend(inputImage);
                 $("#gifs").prepend(inputDiv);
             }
-            console.log(response);
+
+            //console.log(response);
         });
+
+    });
+
+    $(".gif").on("click", function () {
+        console.log("It's been clicked!");
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-still"));
+        $(this).attr("data-state", "still");
+    }
 
     });
 
