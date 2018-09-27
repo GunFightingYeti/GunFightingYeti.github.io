eae7eb814af2621d557b64a66b03198a94ab3e76
Tim Lewis <timothydlewis@icloud.com>
2018-09-26 19:09:00 -0600

    Got seperate file working for omdb call.

--- Assignment8/liri_node_app/liri.js
+++ Assignment8/liri_node_app/liri.js
@@ -2,6 +2,7 @@
 require("dotenv").config();
 // require("keys.js");
 var request = require("request");
+var omdb = require("./omdb.js")
 var Spotify = require('node-spotify-api');
 
 // Input capture
@@ -19,39 +20,6 @@ for (var i = 3; i < nodeArgs.length; i++) {
 }
 
 // API calls
-// OMDB API
-function omdb(movie) {
-
-  var queryURL = "https://www.omdbapi.com/?i=tt3896198&t=" + movie + "&y=&plot=short&apikey=d07256e8";
-
-  request(queryURL, function (error, response, body) {
-
-    // If the request is successful
-    if (!error && response.statusCode === 200) {
-      // console.log("This is the body: " + body);
-
-      var find = (JSON.parse(body));
-
-      var omdb = {
-        title: find.Title,
-        year: find.Year,
-        rating: find.Rated,
-        tomatoes: find.Ratings[1].Source,
-        tomatoes1: find.Ratings[1].Value,
-        country: find.Country,
-        plot: find.Plot,
-        actors: find.Actors
-      }
-
-      // Write the object to the command line
-      console.log("\n" + "Title : " + omdb.title + "\n" + "Release year : " + omdb.year + "\n" + "Rating : " + omdb.rating + "\n" + omdb.tomatoes + " rating: " + omdb.tomatoes1 + "\n" + "Country of origin: " + omdb.country + "\n" + "Plot: " + omdb.plot + "\n" + "Actors : " + omdb.actors);
-
-    } else {
-      console.log("Something went wrong. Try doing better.");
-    }
-  });
-}
-
 // Spotify API
 function spotify(song) {
   // Spotify API
@@ -119,6 +87,7 @@ switch (database) {
     console.log("\nYour search database is 'Spotify'");
     break;
 
+    // Pulled from omdb.js
   case "movie-this":
     omdb(media);
     console.log("\nYour search database is 'OMDB'");
--- Assignment8/liri_node_app/omdb.js
+++ Assignment8/liri_node_app/omdb.js
@@ -1,36 +1,36 @@
 var request = require("request");
 
 
-// Then run a request to the OMDB API with the movie specified
 function omdb(movie) {
 
-    var queryURL = "https://www.omdbapi.com/?i=tt3896198t=" + movie + "&y=&plot=full&apikey=d07256e8";
-
+    var queryURL = "https://www.omdbapi.com/?i=tt3896198&t=" + movie + "&y=&plot=full&apikey=d07256e8";
+  
     request(queryURL, function (error, response, body) {
-
-        // If the request is successful (i.e. if the response status code is 200)
-        // Parse the body of the site and recover just the imdbRating
-        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-
-        // console.log(rating);
-
-        // If the request is successful
-        if (!error && response.statusCode === 200) {
-            console.log(body);
-            console.log(response);
-            var omdb = {
-                // title = response.imdbTitle,
-                // rating = response.imdbRated,
-                // year = response.imdbYear,
-                // plot = response.imdbPlot,
-                // time = response.imdbRuntime,
-            }
-            return omdb;
+  
+      // If the request is successful
+      if (!error && response.statusCode === 200) {
+        // console.log("This is the body: " + body);
+  
+        var find = (JSON.parse(body));
+  
+        var omdb = {
+          title: find.Title,
+          year: find.Year,
+          rating: find.Rated,
+          tomatoes: find.Ratings[1].Source,
+          tomatoes1: find.Ratings[1].Value,
+          country: find.Country,
+          plot: find.Plot,
+          actors: find.Actors
         }
+  
+        // Write the object to the command line
+        console.log("\n" + "Title : " + omdb.title + "\n" + "Release year : " + omdb.year + "\n" + "Rated : " + omdb.rating + "\n" + omdb.tomatoes + " rating: " + omdb.tomatoes1 + "\n" + "Country of origin: " + omdb.country + "\n" + "Plot: " + omdb.plot + "\n" + "Actors : " + omdb.actors);
+  
+      } else {
+        console.log("Something went wrong. Try doing better.");
+      }
     });
-}
-module.exports = {
-    // omdb: omdb,
-};
+  }
 
-// console.log(omdb);
+module.exports = omdb
