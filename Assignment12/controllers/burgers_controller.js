3f3cf5e59d6fe102ebf8bef2b04bdd2b9dcdfaf0
Tim Lewis <timothydlewis@icloud.com>
2018-10-27 09:58:40 -0600

    Margin changes and devoured boolean change

--- Assignment12/controllers/burgers_controller.js
+++ Assignment12/controllers/burgers_controller.js
@@ -29,7 +29,7 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burgers.update({
-    devoured: 1
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       return res.status(404).end();
