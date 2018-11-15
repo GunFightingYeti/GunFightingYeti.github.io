d5ff4e65934b08b4822b4eb94d174a9193114cd5
Tim Lewis <timothydlewis@icloud.com>
2018-11-14 23:10:16 -0700

    Testing keyboard pop-up

--- Assignment3/Word Guess Game/assets/javascript/game.js
+++ Assignment3/Word Guess Game/assets/javascript/game.js
@@ -149,6 +149,6 @@ function restart() {
     guessed.innerHTML = "";
 }
 
-// document.addEventListener("DOMContentLoaded", function(event) { 
-//     document.getElementsByTagName("body").trigger("tap");
-//   });
+document.addEventListener("DOMContentLoaded", function(event) { 
+    document.getElementsByTagName("body").trigger("tap");
+  });
