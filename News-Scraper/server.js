7abc748bdd90ac6810b56f4b82009bb04118a130
Tim Lewis <timothydlewis@icloud.com>
2018-11-19 22:14:59 -0700

    Updated server.js and package.json for heroku

--- News-Scraper/server.js
+++ News-Scraper/server.js
@@ -10,7 +10,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
